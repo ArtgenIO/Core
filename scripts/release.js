d130f6e2aee0a42c4334e916865cf2ef707057ee
Varga Zsolt <hello@hisorange.me>
2021-12-01 13:46:31 +0100

    Script to manage release bumping

---
+++ scripts/release.js
@@ -0,0 +1,44 @@
+const { Select } = require('enquirer');
+const { readFileSync, writeFileSync, write } = require('fs');
+const { join } = require('path');
+const { parse, stringify } = require('semver-utils');
+const { execSync } = require('child_process');
+
+const prompt = new Select({
+  name: 'semver',
+  message: 'Choose a version change level (semver):',
+  choices: ['patch', 'minor', 'major'],
+});
+
+prompt
+  .run()
+  .then(async answer => {
+    const vfile = join(__dirname, '../version');
+    const pfile = join(__dirname, '../package.json');
+    const current = readFileSync(vfile).toString();
+    console.log('Current version:', current);
+
+    const semver = parse(current);
+
+    semver[answer] = parseInt(semver[answer], 10) + 1;
+    const bumped = stringify(semver);
+    console.log('New version:', bumped);
+
+    const package = require(pfile);
+    package.version = bumped;
+
+    writeFileSync(pfile, JSON.stringify(package, null, 2));
+    writeFileSync(vfile, bumped);
+
+    console.log('Version updated');
+
+    execSync(
+      `git add version package.json && git commit -m "Release ${bumped}"`,
+      {
+        cwd: join(__dirname, '../'),
+      },
+    );
+
+    console.log('Commit made for the automatic release');
+  })
+  .catch(console.error);
