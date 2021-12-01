66d7c2f7b3913a7d291fabe3a72891f9267be164
Varga Zsolt <hello@hisorange.me>
2021-12-01 13:28:34 +0100

    Timed restart in demo mode

--- src/main.ts
+++ src/main.ts
@@ -34,6 +34,11 @@ export async function main(modules: Constructor<IModule>[]): Promise<IKernel> {
       // Register the shutdown hooks.
       process.on('SIGINT', shutdown.bind(shutdown));
       process.on('SIGTERM', shutdown.bind(shutdown));
+
+      // Demo mode restarts every 30 minute, it rebuilds the database in case someone breaks it, yep Norbi, I mean You!... :D
+      if (process.env?.ARTGEN_DEMO === '1') {
+        setTimeout(() => kernel.stop(), 1_800_000);
+      }
     } else {
       process.exit(3);
     }
