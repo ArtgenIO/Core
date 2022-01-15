8cd21c18d8506bfe9b709571f9f6701816ba8313
Varga Zsolt <hello@hisorange.me>
2022-01-15 20:25:11 +0100

    Temp paginator

--- src/modules/content/component/grid.component.tsx
+++ src/modules/content/component/grid.component.tsx
@@ -1,14 +1,14 @@
 import {
+  DeleteOutlined,
   FileOutlined,
   FilterOutlined,
   ReloadOutlined,
-  RollbackOutlined,
 } from '@ant-design/icons';
 import { ColumnApi, GridApi } from 'ag-grid-community';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham-dark.css';
 import { AgGridColumnProps, AgGridReact } from 'ag-grid-react';
-import { Button, message } from 'antd';
+import { Button, message, Pagination } from 'antd';
 import { debounce } from 'lodash';
 import { QueryBuilder } from 'odata-query-builder';
 import React, { useEffect, useState } from 'react';
@@ -34,7 +34,8 @@ export default function GridComponent({ schema }: Props) {
   const [showEdit, setShowEdit] = useState<RowLike>(null);
 
   // Pagination
-  const [perPage, setPerPage] = useState(25);
+  const [pageSize, setPageSize] = useState(20);
+  const [pageCurr, setPageCurr] = useState(1);
 
   // AG Grid API access
   const [gridApi, setGridApi] = useState<GridApi>(null);
@@ -47,7 +48,7 @@ export default function GridComponent({ schema }: Props) {
   // Load initial content
   const [{ data: content, loading: isContentLoading }, __do_fetch] =
     useHttpClient<object[]>(
-      toRestRoute(schema) + new QueryBuilder().top(perPage).toQuery(),
+      toRestRoute(schema) + new QueryBuilder().top(pageSize).toQuery(),
     );
 
   // Rebuild the request and fetch the new data
@@ -55,7 +56,11 @@ export default function GridComponent({ schema }: Props) {
     const qb = new QueryBuilder();
 
     // Pagination limit
-    qb.top(perPage);
+    qb.top(pageSize);
+
+    if (pageCurr > 1) {
+      qb.skip(pageCurr * pageSize - pageSize);
+    }
 
     // Sorting
     for (const col of columnApi.getAllColumns()) {
@@ -72,6 +77,12 @@ export default function GridComponent({ schema }: Props) {
       url: toRestRoute(schema) + qb.toQuery(),
     });
   };
+
+  useEffect(() => {
+    if (!isContentLoading) {
+      refetch();
+    }
+  }, [pageCurr, pageSize]);
 
   const rebuildMeta = () => {
     if (columnApi) {
@@ -138,9 +149,9 @@ export default function GridComponent({ schema }: Props) {
       let sortable = true;
       let resizable = true;
       let cellRenderer: AgGridColumnProps['cellRenderer'];
-      let initialWidth: AgGridColumnProps['initialWidth'];
-      let minWidth: AgGridColumnProps['minWidth'];
-      let maxWidth: AgGridColumnProps['maxWidth'];
+      let initialWidth: AgGridColumnProps['initialWidth'] = undefined;
+      let minWidth: AgGridColumnProps['minWidth'] = undefined;
+      let maxWidth: AgGridColumnProps['maxWidth'] = undefined;
       const cellClass: AgGridColumnProps['cellClass'] = [];
 
       // UUID rendering
@@ -179,7 +190,7 @@ export default function GridComponent({ schema }: Props) {
           return `<code class="bg-midnight-600 p-0.5 rounded-md"
             >${
               params.value
-                ? JSON.stringify(params.value).substring(0, 8)
+                ? JSON.stringify(params.value).substring(0, 32)
                 : 'null'
             }</code>`;
         };
@@ -191,11 +202,12 @@ export default function GridComponent({ schema }: Props) {
 
       const fieldDef: AgGridColumnProps = {
         field: field.reference,
+        filter: true,
+        floatingFilter: true,
         headerName: field.title,
         resizable,
-        initialWidth,
-        minWidth,
-        maxWidth,
+        minWidth: minWidth ?? 150,
+        maxWidth: 500,
         cellRenderer,
         sortable,
         cellClass,
@@ -227,15 +239,13 @@ export default function GridComponent({ schema }: Props) {
     columnDef.push({
       headerName: 'Actions',
       field: null,
-      width: 90,
-      maxWidth: 90,
+      maxWidth: 250,
       sortable: false,
-      resizable: false,
-      suppressAutoSize: true,
-      suppressSizeToFit: true,
       suppressMovable: true,
       filter: false,
+      headerClass: ['text-center'],
       cellClass: ['text-center'],
+      pinned: 'right',
       cellRenderer: () => {
         return '[D]';
       },
@@ -251,20 +261,47 @@ export default function GridComponent({ schema }: Props) {
 
   return (
     <>
-      <div className="flex flex-row-reverse">
-        <Button.Group className="mb-2">
-          <Button icon={<FileOutlined />} onClick={() => setShowCreate(true)}>
-            New
-          </Button>
-          <Button icon={<FilterOutlined />}>Filter</Button>
-          <Button icon={<ReloadOutlined />} onClick={() => refetch()}>
-            Reload
-          </Button>
-          <Button icon={<RollbackOutlined />}>Reset</Button>
-        </Button.Group>
+      <div className="flex">
+        <div className="grow">
+          <Button.Group className="mb-3">
+            <Button icon={<FileOutlined />} onClick={() => setShowCreate(true)}>
+              New
+            </Button>
+            <Button icon={<FilterOutlined />}>Filter</Button>
+            <Button icon={<ReloadOutlined />} onClick={() => refetch()}>
+              Reload
+            </Button>
+            <Button icon={<DeleteOutlined />} disabled>
+              Delete
+            </Button>
+          </Button.Group>
+        </div>
+
+        <div className="shrink text-right">
+          <Pagination
+            total={5000}
+            defaultCurrent={pageCurr}
+            current={pageCurr}
+            pageSize={pageSize}
+            pageSizeOptions={[10, 20, 50, 100, 500, 1000]}
+            showSizeChanger
+            showQuickJumper
+            showTotal={total => (
+              <span>
+                Total{' '}
+                <span className="text-green-500">{total.toLocaleString()}</span>{' '}
+                record
+              </span>
+            )}
+            onChange={(_pageNth, _pageSize) => {
+              setPageCurr(_pageNth);
+              setPageSize(_pageSize);
+            }}
+          />
+        </div>
       </div>
 
-      <div className="ag-theme-balham-dark w-full" style={{ height: 600 }}>
+      <div className="ag-theme-balham-dark w-full" style={{ height: 780 }}>
         <AgGridReact
           reactUi={true}
           columnDefs={columns}
